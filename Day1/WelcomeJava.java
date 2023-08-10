bafe0c6dbf31bfed2e7be26fd1f741b6f35cc45c
techzenure <141934253+techzenure@users.noreply.github.com>
2023-08-10 20:47:55 +0530

    Add files via upload

---
+++ Day1/WelcomeJava.java
@@ -0,0 +1,15 @@
+package Day1;
+public class WelcomeJava {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to India");
+		System.out.print("by ");
+		System.out.println("Team @ Techzenure");
+	}
+
+}
+
+
+	
+
+
